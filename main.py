c94baf57bb14e3a5fb6915e16fe72925a2eadbda
Duc Pham <ducpmm@gmail.com>
2024-01-09 08:16:16 -0500

    Refactor discord.py and main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # from assistant import generate_text
-from discord import get_message, send_message
+from discord import send_message
 from web_ui import launch
 from actor import predict_from_directory
 
-async def on_submit(directory, channel, actor_image):
+def on_submit(directory, channel, actor_image):
     if directory is not None:
         names = predict_from_directory(directory)
-
+        send_message(names)
         return names
 
     return "Please input a directory"
